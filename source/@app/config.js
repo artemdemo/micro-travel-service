c99e2dab636c1baea8ffb1d8f0ad5a2f6d857f3a
Artem Demo <artemdemo@gmail.com>
2019-02-17 08:48:04 +0200

    updated apps names

--- source/@app/config.js
+++ source/@app/config.js
@@ -4,6 +4,6 @@
  * This is inner configuration that can be changed only in the build.
  */
 
-export const availableApps = ['bus', 'air'];
-export const baseUrlBus = '/bus';
-export const baseUrlAir = '/air';
+export const availableApps = ['train', 'avia'];
+export const baseUrlTrain = '/train';
+export const baseUrlAvia = '/avia';
